6c7bb9b0e65dbf9f00cb2f0fe8d9c0c0432e3c30
swcho25 <dkf1141@gmail.com>
2026-05-29 16:45:40 +0900

    initial commit

---
+++ src/main/java/com/partion/PartionApplication.java
@@ -0,0 +1,13 @@
+package com.partion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partion/PartionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
